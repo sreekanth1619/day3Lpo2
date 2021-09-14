e6e8e8aed2e9341650209bcecc39d6ef1e25330a
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-09-14 11:01:41 +0530

    comparing the lines

---
+++ EqualLength.java
@@ -0,0 +1,24 @@
+package assignment1B;
+
+public class EqualLength {
+	public static void main(String[] args) {
+		 int x1,x2,y1,y2;
+	     x1=1;x2=4;y1=1;y2=4;
+	     double dis;
+	     double length;
+	     length=Math.sqrt((y2-y1)*(y2-y1) + (x2-x1)*(x2-x1));
+	     dis=Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+	     System.out.println("length of two points:" +length);
+
+	    System.out.println("the length of the line is:"+"("+x2+","+x1+"),"+"("+y2+","+y1+")====>" +dis);
+	    
+	  if(+length == +dis) {
+		  System.out.println("given two length is equal:");
+	  }
+	  
+	  if(+length != +dis) {		 		  
+		System.out.println("given two length is not equal" );	  
+		  
+	  }
+}
+}
